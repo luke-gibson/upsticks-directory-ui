e89c87af15ff9f8d1e1d34d63b6161506c77b437
luke-gibson <hello@lukegibsondesign.co.uk>
2024-07-04 16:55:41 +0100

    added hero image component start

---
+++ components/HeroComponent.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+    const props = defineProps({
+        large: {
+            type: String,
+            required: true
+        },
+        medium: {
+            type: String,
+            required: true
+        },
+        small: {
+            type: String,
+            required: true
+        }
+    });
+</script>
+
+<template>
+    <figure class="relative w-full  bg-gray-800 overflow-hidden">
+        <picture>
+            <source :srcset="large" media="(min-width: 1024px)">
+            <source :srcset="medium" media="(min-width: 768px)">
+            <img :src="small" alt="Hero Image" class="w-full object-cover">
+        </picture>
+    </figure>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
+  <HeroComponent
+      large="https://res.cloudinary.com/dqqarxqbu/image/upload/v1613488106/Luke%20Gibson%20Design/banner-large-32-15_pzhu9y.jpg"
+      medium="https://res.cloudinary.com/dqqarxqbu/image/upload/v1720099452/upsticks%20diredtory/tablet-home-page_xfrd3l.jpg"
+      small="https://res.cloudinary.com/dqqarxqbu/image/upload/v1720099452/upsticks%20diredtory/mobile-home-page_crptxj.jpg"
+    />
   <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900">ABOUT PAGE</h1>
 </template>
