efe0907c1507609cdbe0b7ef7800161438c3bfe4
luke-gibson <hello@lukegibsondesign.co.uk>
2024-07-05 22:09:03 +0100

    change font size for links in header

--- components/layout/NavigationComponent.vue
+++ components/layout/NavigationComponent.vue
@@ -4,10 +4,10 @@
   <nav>
     <ul class="flex">
         <li class="mr-6">
-            <NuxtLink class="text-gray-800 hover:text-[#E2883C] transition-colors duration-300 font-bold flex items-center" to="/log-in"><span class="mr-0 md:mr-2"><IconsLogin></IconsLogin></span> <span class="hidden md:block">Log in</span></NuxtLink>
+            <NuxtLink class="text-gray-800 hover:text-[#E2883C] transition-colors duration-300 font-bold flex items-center" to="/log-in"><span class="mr-0 md:mr-2"><IconsLogin></IconsLogin></span> <span class="hidden md:block text-sm">Log in</span></NuxtLink>
         </li>
         <li>
-            <NuxtLink class="text-gray-800 hover:text-[#E2883C] transition-colors duration-300 font-bold flex items-center" to="/business-registration"><span class="mr-0 md:mr-2"><IconsAddUser></IconsAddUser></span> <span class="hidden md:block">Business Registration</span></NuxtLink>
+            <NuxtLink class="text-gray-800 hover:text-[#E2883C] transition-colors duration-300 font-bold flex items-center" to="/business-registration"><span class="mr-0 md:mr-2"><IconsAddUser></IconsAddUser></span> <span class="hidden md:block text-sm">Business Registration</span></NuxtLink>
         </li>
     </ul>
   </nav>
