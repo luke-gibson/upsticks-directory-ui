56f648f341866e0e6b9053ea96563645a7f16bc6
Ian Jamieson <i@njamieson.co.uk>
2024-07-09 13:32:42 +0100

    upate search results page

--- components/Search.vue
+++ components/Search.vue
@@ -10,7 +10,7 @@ const searchQuery = ref('');
         placeholder="Enter postcode, town or city"
         class="w-full sm:w-96 bg-gray-50 py-3.5 px-4 text-[#333] text-base focus:outline-none rounded"
     >
-    <NuxtLink :to="`/search-results?q=${searchQuery}`" class="max-sm:mt-8 sm:ml-4 bg-[#E2883C] hover:bg-[#be7335] text-[#2B5773] text-base font-semibold py-3.5 px-6 rounded hover:shadow-md hover:transition-transform transition-transform hover:scale-105 focus:outline-none">
+    <NuxtLink :to="`/search?q=${searchQuery}`" class="max-sm:mt-8 sm:ml-4 bg-[#E2883C] hover:bg-[#be7335] text-[#2B5773] text-base font-semibold py-3.5 px-6 rounded hover:shadow-md hover:transition-transform transition-transform hover:scale-105 focus:outline-none">
         Search
     </NuxtLink>
 </template>
